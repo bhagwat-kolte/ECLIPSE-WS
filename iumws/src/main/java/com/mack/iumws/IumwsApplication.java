2c0cccc71e31cbe9f32555e278f484566bfba070
Bhagwat Kolte <mack37@gmail.com>
2023-09-16 01:48:11 +0530

    Initial Commit
    new project

---
+++ iumws/src/main/java/com/mack/iumws/IumwsApplication.java
@@ -0,0 +1,13 @@
+package com.mack.iumws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IumwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IumwsApplication.class, args);
+	}
+
+}
---
+++ iumws/src/test/java/com/mack/iumws/IumwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mack.iumws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IumwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
